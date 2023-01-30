2d9abb870aaedd6c290ee222ddb8265290c90145
Rama <ramamrcew@gmail.com>
2023-01-30 18:23:40 +0530

    inital project

---
+++ src/main/java/com/rs/tuition/TuitionApplication.java
@@ -0,0 +1,13 @@
+package com.rs.tuition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TuitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TuitionApplication.class, args);
+	}
+
+}
