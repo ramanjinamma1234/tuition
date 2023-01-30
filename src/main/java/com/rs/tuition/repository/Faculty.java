20a870247ef8d06cee4ed770f53871bf93f35b22
Rama <ramamrcew@gmail.com>
2023-01-30 21:04:09 +0530

    sample text file added

--- src/main/java/com/rs/tuition/repository/Faculty.java
+++ src/main/java/com/rs/tuition/repository/Faculty.java
@@ -1,7 +1,19 @@
 package com.rs.tuition.repository;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.ToString;
 
 @Entity
+@Data
+@ToString
 public class Faculty {
+    @Id
+    private String id;
+    private String firstname;
+    private String lastName;
+    private String password;
+    private long phno;
+
 }
